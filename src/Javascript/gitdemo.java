86952c243d8539c56facd76a393fe415c0c4ebab
ELCOT <ELCOT@Lenovo>
2022-07-18 12:36:13 -0700

    first commit

---
+++ src/Javascript/gitdemo.java
@@ -0,0 +1,12 @@
+package Javascript;
+
+public class gitdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="message from head";
+		System.out.println(str);
+
+	}
+
+}
